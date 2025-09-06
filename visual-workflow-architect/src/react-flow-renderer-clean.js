414bbd0d82437704de585be9a0ea16ba06e6936e
Rahul Gupta <iamrahulgupta4u@gmail.com>
2025-09-07 05:00:31 +0530

    Testing Self Assignment test 3

--- visual-workflow-architect/src/react-flow-renderer-clean.js
+++ visual-workflow-architect/src/react-flow-renderer-clean.js
@@ -409,7 +409,7 @@ window.SequenceDiagramRenderer = {
 
       .self-message-path {
         position: relative;
-        width: 70px;
+        width: 50px; /* Reduced from 70px to prevent extending into workflow backgrounds */
         height: 100%;
       }
       
@@ -421,23 +421,31 @@ window.SequenceDiagramRenderer = {
       }
 
       .self-message-path-top {
-        width: 100%; height: 2px; top: 0; left: 1px;
+        width: 50px; height: 2px; top: 0; left: 19px; /* Start from circle edge (19px radius) */
       }
 
       .self-message-path-top::after {
-        content: ''; position: absolute; right: -1px; top: -4px; border-top: 5px solid transparent; border-bottom: 5px solid transparent; border-left: 8px solid #555;
+        content: ''; position: absolute; right: -1px; top: -4px; 
+        width: 0; height: 0;
+        border-top: 5px solid transparent; 
+        border-bottom: 5px solid transparent; 
+        border-left: 8px solid #555;
       }
 
       .self-message-path-vertical {
-        width: 2px; height: 100%; top: 0; left: 71px;
+        width: 2px; height: 100%; top: 0; left: 69px; /* Adjusted for 50px width */
       }
 
       .self-message-path-bottom {
-        width: 100%; height: 2px; bottom: 0; left: 1px;
+        width: 50px; height: 2px; bottom: 0; left: 19px; /* Start from circle edge */
       }
 
       .self-message-path-bottom::after {
-        content: ''; position: absolute; left: -1px; top: -4px; border-top: 5px solid transparent; border-bottom: 5px solid transparent; border-right: 8px solid #555;
+        content: ''; position: absolute; left: -1px; top: -4px; 
+        width: 0; height: 0;
+        border-top: 5px solid transparent; 
+        border-bottom: 5px solid transparent; 
+        border-right: 8px solid #555;
       }
   .add-container-btn {
         width: 24px;
